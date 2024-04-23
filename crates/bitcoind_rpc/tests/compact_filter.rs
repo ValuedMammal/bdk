23907325c6b7f009b4babda3128c129a36f91b89
valued mammal <valuedmammal@protonmail.com>
2024-04-22 20:54:42 -0400

    Update tests

--- crates/bitcoind_rpc/tests/compact_filter.rs
+++ crates/bitcoind_rpc/tests/compact_filter.rs
@@ -1,4 +1,3 @@
-use anyhow::Context;
 use bdk_bitcoind_rpc::compact_filter;
 use bdk_chain::keychain::KeychainTxOutIndex;
 use bdk_chain::local_chain;
@@ -7,63 +6,53 @@ use bdk_chain::miniscript::Descriptor;
 use bdk_chain::ConfirmationTimeHeightAnchor;
 use bdk_chain::IndexedTxGraph;
 use bdk_testenv::anyhow::Result;
+use bdk_testenv::bitcoind;
 use bdk_testenv::bitcoind::BitcoinD;
 use bdk_testenv::bitcoind::Conf;
 use bitcoin::constants::genesis_block;
 use bitcoin::secp256k1::Secp256k1;
 use bitcoin::{Address, Amount, BlockHash, Network, Txid};
 use bitcoincore_rpc;
-use bitcoincore_rpc::Auth;
 use bitcoincore_rpc::RpcApi;
 
 #[allow(unused)]
 struct TestEnv {
     bitcoind: BitcoinD,
-    client: bitcoincore_rpc::Client,
 }
 
 impl TestEnv {
     fn new() -> Result<Self> {
-        let client = new_client()?;
         let mut conf = Conf::default();
         conf.args.push("-blockfilterindex=1");
         conf.args.push("-peerblockfilters=1");
-        let exe = std::env::var("TEST_BITCOIND")?;
+        let exe = std::env::var("TEST_BITCOIND").unwrap_or(bitcoind::downloaded_exe_path()?);
         let bitcoind = BitcoinD::with_conf(exe, &conf)?;
 
-        Ok(Self { bitcoind, client })
+        Ok(Self { bitcoind })
     }
 
-    fn client(&self) -> &bitcoincore_rpc::Client {
-        &self.client
+    fn client(&self) -> &impl bitcoincore_rpc::RpcApi {
+        &self.bitcoind.client
     }
 
     fn mine_blocks(&self, blocks: u64, addr: &Address) -> Result<Vec<BlockHash>> {
-        Ok(self.client.generate_to_address(blocks, addr)?)
+        Ok(self.client().generate_to_address(blocks, addr)?)
     }
 
     fn send_tx(&self, address: &Address, amount: Amount) -> Result<Txid> {
         Ok(self
-            .client
+            .client()
             .send_to_address(address, amount, None, None, None, None, Some(1), None)?)
     }
 
     fn block_until_height(&self, height: u64) -> Result<()> {
         let mut delay = std::time::Duration::from_millis(64);
-        while self.client.get_block_count()? < height {
+        while self.client().get_block_count()? < height {
             std::thread::sleep(delay);
             delay *= 2;
         }
         Ok(())
     }
-}
-
-fn new_client() -> Result<bitcoincore_rpc::Client> {
-    let path = std::env::var("RPC_COOKIE").context("must set RPC_COOKIE env var")?;
-    Ok(bitcoincore_rpc::Client::new(
-        "127.0.0.1:18443",
-        Auth::CookieFile(path.into()),
-    )?)
 }
 
 /// Test keychain kind
@@ -76,15 +65,21 @@ enum Keychain {
 
 const DESC: &str = "tr([7d94197e/86'/1'/0']tpubDCyQVJj8KzjiQsFjmb3KwECVXPvMwvAxxZGCP9XmWSopmjW3bCV3wD7TgxrUhiGSueDS1MU5X1Vb1YjYcp8jitXc5fXfdC1z68hDDEyKRNr/0/*)";
 
-/// Given a new LocalChain, and a remote tip height of 101,
-/// when we send a tx from the mining node to the receiver
-/// expect:
-///     local chain is synced to the new tip
-///     new anchors inserted into graph
-///     tx graph shows a confirmed balance equal to the send amt
-///     keychain indices updated
+/// Given initial params:
+///     - keychain, descriptor, and target_index
+///     - remote tip height of 101,
+///
+/// When we send a tx from the mining node to the receiver,
+/// expect chain changeset:
+///     - contains the new blocks
+///
+/// expect indexed_tx_graph changeset:
+///     - includes sent tx
+///     - new confirmation height anchor
+///     - last_revealed keychain index
+///
+/// finally, expect computed balance equals send amt
 #[test]
-#[ignore]
 fn test_sync() -> Result<()> {
     let env = TestEnv::new()?;
     let core = env.client();
@@ -133,34 +128,45 @@ fn test_sync() -> Result<()> {
     } = client.sync()?;
 
     // Apply updates
-    let _ = chain.apply_update(local_chain::Update {
+    let chain_changeset = chain.apply_update(local_chain::Update {
         tip,
         introduce_older_blocks: true,
     })?;
-    let _ = graph.apply_changeset(indexed_tx_graph.initial_changeset());
 
     // chain updated
-    assert!(chain.iter_checkpoints().collect::<Vec<_>>().len() > 1);
+    assert_eq!(chain_changeset.len(), 10);
     let cp = chain.tip();
     assert_eq!(cp.height(), remote_tip_height);
 
-    // graph populated with tx data
-    let indexed_outpoints = graph.index.outpoints().clone();
-    assert!(!indexed_outpoints.is_empty());
-    let op = indexed_outpoints.iter().next().unwrap().1;
-    assert_eq!(op.txid, sent_txid);
-    let balance = graph
-        .graph()
-        .balance(&chain, cp.block_id(), indexed_outpoints, |_, _| true);
-    assert_eq!(balance.confirmed, send_amt.to_sat());
-    let (anchor, _) = graph.graph().all_anchors().iter().next().unwrap();
-    assert_eq!(anchor.confirmation_height, remote_tip_height);
+    // new tx added
+    let graph_init_changeset = indexed_tx_graph.initial_changeset();
+    let tx = graph_init_changeset.graph.txs.iter().next().unwrap();
+    let txout = tx
+        .output
+        .iter()
+        .find(|txo| txo.script_pubkey == spk.to_owned())
+        .unwrap();
+    assert_eq!(txout.value, send_amt);
 
-    // keychains updated
-    assert_eq!(
-        graph.index.last_revealed_index(&keychain),
-        Some(target_index)
+    // new anchor added
+    let (anchor, txid) = graph_init_changeset.graph.anchors.iter().next().unwrap();
+    assert_eq!(anchor.confirmation_height, 102);
+    assert_eq!(txid, &sent_txid);
+
+    // last revealed of keychain equal to `target_index`
+    let (k, last_revealed) = graph_init_changeset.indexer.0.iter().next().unwrap();
+    assert_eq!(k, &keychain);
+    assert_eq!(last_revealed, &target_index);
+    let _ = graph.apply_changeset(graph_init_changeset);
+
+    // balance updated
+    let balance = graph.graph().balance(
+        &chain,
+        cp.block_id(),
+        graph.index.outpoints().clone(),
+        |_, _| true,
     );
+    assert_eq!(balance.confirmed, send_amt.to_sat());
 
     Ok(())
 }
